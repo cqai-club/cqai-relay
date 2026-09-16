nemozhu8563/New-API-plus

fix: 修正火山方舟渠道获取模型列表的端点路径 (#7203)
VolcEngine (type=45) 渠道 fetch models 时拼接 {base}/v1/models，
但火山方舟 OpenAI 兼容 API 根路径是 /api/v3，不存在 /v1/models，
导致「获取模型列表」固定返回 404。改为 {base}/api/v3/models。

controller/channel_upstream_update.go
@@ -417,10 +417,13 @@ func fetchChannelUpstreamModelIDs(channel *model.Channel) ([]string, error) {
 			url = fmt.Sprintf("%s/api/paas/v4/models", baseURL)
 		}
 	case constant.ChannelTypeVolcEngine:
+		// 火山方舟 OpenAI 兼容 API 根路径是 /api/v3（chat/embeddings 均为
+		// {base}/api/v3/...，见 relay/channel/volcengine 的 GetRequestURL），
+		// 不存在 /v1/models 端点。原拼接会导致「获取模型列表」固定 404 失败。
 		if plan, ok := constant.ChannelSpecialBases[baseURL]; ok && plan.OpenAIBaseURL != "" {
 			url = fmt.Sprintf("%s/v1/models", plan.OpenAIBaseURL)
 		} else {
-			url = fmt.Sprintf("%s/v1/models", baseURL)
+			url = fmt.Sprintf("%s/api/v3/models", baseURL)
 		}
 	case constant.ChannelTypeMoonshot:
 		if plan, ok := constant.ChannelSpecialBases[baseURL]; ok && plan.OpenAIBaseURL != "" {

cqai-club/cqai-relay

controller/channel_upstream_update.go
@@ -441,10 +441,13 @@ func fetchChannelUpstreamModels(channel *model.Channel) (channelUpstreamModelCat
 			requestURL = fmt.Sprintf("%s/api/paas/v4/models", baseURL)
 		}
 	case constant.ChannelTypeVolcEngine:
+		// 火山方舟 OpenAI 兼容 API 根路径是 /api/v3（chat/embeddings 均为
+		// {base}/api/v3/...，见 relay/channel/volcengine 的 GetRequestURL），
+		// 不存在 /v1/models 端点。原拼接会导致「获取模型列表」固定 404 失败。
 		if plan, ok := constant.ChannelSpecialBases[baseURL]; ok && plan.OpenAIBaseURL != "" {
 			requestURL = fmt.Sprintf("%s/v1/models", plan.OpenAIBaseURL)
 		} else {
-			requestURL = fmt.Sprintf("%s/v1/models", baseURL)
+			requestURL = fmt.Sprintf("%s/api/v3/models", baseURL)
 		}
 	case constant.ChannelTypeMoonshot:
 		if plan, ok := constant.ChannelSpecialBases[baseURL]; ok && plan.OpenAIBaseURL != "" {